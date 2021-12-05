630c950ebc7382607746dcac82adfe54094cd69c
Andres <andresr774@gmail.com>
2021-12-05 16:01:41 -0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Andres camilo Raigoza misas on 5/12/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
